88746351f2800ed3d65da8bb6f734eed17893c66
Adarsh singh <adarshsingh345125@gmail.com>
2022-05-27 22:16:43 +0530

    Updates

---
+++ precache-manifest.cb4e0b08e36cb76d27b164cbb9028f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f70c160f20f7d060bb0b",
+    "url": "/TextUtils/static/css/main.f8f8fad5.chunk.css"
+  },
+  {
+    "revision": "f70c160f20f7d060bb0b",
+    "url": "/TextUtils/static/js/main.f70c160f.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "039c934be358153dd9d0",
+    "url": "/TextUtils/static/js/2.039c934b.chunk.js"
+  },
+  {
+    "revision": "94fa866583836ad85178",
+    "url": "/TextUtils/static/js/runtime~main.94fa8665.js"
+  },
+  {
+    "revision": "f9c9ec3383fe1dfa0ba6ec2d99e3df89",
+    "url": "/TextUtils/index.html"
+  }
+];
